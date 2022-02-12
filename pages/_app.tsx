146a1d887bd965c2cd7b5d4c6ebfaad3216551d8
ayla~nonsense <4943384+aylanonsense@users.noreply.github.com>
2022-02-11 19:39:34 -0500

    Responsive game grid

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import 'styles/globals.scss'
 const PortfolioApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
-      <meta name="viewport" content="viewport-fit=cover" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <Component {...pageProps} />
   </>
